995aef5a5cb548baee6345589cd97dd5b95507ec
Flavie-LHP5 <name>
2021-01-22 09:49:50 +0100

    mise en place des fichiers

---
+++ fifa_data.php
@@ -0,0 +1,42 @@
+<?php 
+$fifa_data = [ 
+    
+    [ 
+    'images' => 'Golo.jpeg',
+    'Name' => 'Zlatan Ibrahimović',
+    'Team' => 'AC Milan',
+    'Nationality' => 'Suédois',
+    'Number' => 11,
+    'Poste' => 'Avant-centre',
+    'Age' => 39
+    ],
+
+    [
+    'Name' => 'N Golo Kante',
+    'Team' => 'Chelsea FC',
+    'Nationality' => 'Francais',
+    'Number' => 7,
+    'Poste' => 'Milieu',
+    'Age' => 29
+    ],
+    [
+    'Name' => 'Juninho',
+    'Team' => 'Olympique Lyonnais',
+    'Nationality' => 'Brésilien',
+    'Number' => 8,
+    'Poste' => 'Milieu',
+    'Age' => 45
+    ],
+    [
+    'Name' => 'Mohamed Salah',
+    'Team' => 'Liverpool FC',
+    'Nationality' => 'Égyptien',
+    'Number' => 8,
+    'Poste' => 'Ailier droit',
+    'Age' => 28
+    ],
+];
+
+?>
+
+
